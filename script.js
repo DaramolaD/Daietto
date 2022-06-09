887b15d9797f64b8c804504fb9ba2cd01c9de138
DARAMOLA DANIEL <dandaramola.d@gmail.com>
2022-06-09 03:51:30 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+
+const navMenu = document.getElementById("nav_menu"),
+     navToggle = document.getElementById("nav_toggle"),
+     navClose = document.getElementById("nav_close");
+
+
+/*=============== SHOW MENU ===============*/
+/* validate if constant exists  */
+if(navToggle) {
+    navToggle.addEventListener("click", () => {
+        navMenu.classList.add("show-menu")
+    })
+}
+if(navClose) {
+    navClose.addEventListener("click", () => {
+        navMenu.classList.remove("show-menu")
+    })
+}
+
+const navLink = document.querySelectorAll(".nav_links");
+
+function linkAction() {
+    navMenu.classList.remove("show-menu")
+}
+
+navLink.forEach(n => n.addEventListener("click", linkAction))
